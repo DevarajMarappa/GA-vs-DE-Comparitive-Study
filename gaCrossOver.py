bd9786bfc42b4c9a3ef60dc8eacbfb9feea16e8b
DevarajMarappa <98761686+DevarajMarappa@users.noreply.github.com>
2023-05-20 23:14:41 -0400

    Update gaCrossOver.py

--- gaCrossOver.py
+++ gaCrossOver.py
@@ -41,8 +41,8 @@ def mutation(individual):
 def ga_logic(problem_dimension,bmFunc):
     global prblm_dim, max_nfc
      # Best Cost of Iterations
-    bestcost = np.empty(number_of_runs)
-
+    #bestcost = np.empty(number_of_runs)
+    fitness_values = []
     prblm_dim = problem_dimension
     max_nfc = 3000 * problem_dimension
 
@@ -51,40 +51,36 @@ def ga_logic(problem_dimension,bmFunc):
         population = initialize_population(pop_size, prblm_dim)
 
         nfc = 0  # Number of fitness evaluations
-        try:
-            while nfc < max_nfc:
-                evaluated_population = [(individual, BenchmarkFunctions.evaluate_functions(bmFunc,individual)) for individual in population]
-                # To get the current function name for plot purpose
-            # _,current_func_name= BenchmarkFunctions.evaluate_functions(bmFunc,None)
-                current_func_name=""
-                evaluated_population.sort(key=lambda x:x[1], reverse=True)
-                population = [individual for individual, _ in evaluated_population]
-                # Crossover and mutation
-                new_population = []
-                for i in range(0, pop_size, 2):
-                    parent1 = population[i]
-                    parent2 = population[i + 1] if i + 1 < pop_size else population[0]
-                    child1, child2 = crossoverGA(parent1, parent2)
-                    mutation(child1)
-                    mutation(child2)
-                    new_population.extend([child1, child2])
-                    nfc += 2  # Increment the fitness evaluation counter
-                    if nfc >= max_nfc:
-                        break
-                population = new_population
+        
+        while nfc < max_nfc:
+            evaluated_population = [(individual, BenchmarkFunctions.evaluate_functions(bmFunc,individual)) for individual in population]
+            # To get the current function name for plot purpose
+            _,current_func_name=BenchmarkFunctions.evaluate_functions(bmFunc,None)
+            evaluated_population.sort(key=lambda x:x[1], reverse=True)
+            population = [individual for individual, _ in evaluated_population]
+            # Crossover and mutation
+            new_population = []
+            for i in range(0, pop_size, 2):
+                parent1 = population[i]
+                parent2 = population[i + 1] if i + 1 < pop_size else population[0]
+                child1, child2 = crossoverGA(parent1, parent2)
+                mutation(child1)
+                mutation(child2)
+                new_population.extend([child1, child2])
+                nfc += 2  # Increment the fitness evaluation counter
+                if nfc >= max_nfc:
+                    break
+            population = new_population
 
-            best_solution = evaluated_population[0][0]
-            best_fitness = evaluated_population[0][1]
-            print(f"Run {run + 1}: Best Solution: {best_solution}, Best Fitness: {best_fitness}")
-
-            # Output
-            out = structure()
-            out.pop = new_population
-            out.bestsol = best_solution
-            out.bestcost = best_fitness
-            out.dim=problem_dimension
-            out.benchMarkFuncName=current_func_name
-            return out
-        except Exception :
-            print("Exception Occurred:"+ str(Exception))
-            break
+        best_solution = evaluated_population[0][0]
+        best_fitness = evaluated_population[0][1]
+        fitness_values.append(evaluated_population[0][1][0])
+    print(f"Run {run + 1}: Best Solution: {best_solution}, Best Fitness: {best_fitness}")
+    generations = range(1, number_of_runs + 1)
+    # Plot the best fitness value over generations
+    plt.plot(generations, fitness_values)
+    plt.xlabel('Generation')
+    plt.ylabel('Best Fitness')
+    plt.title('GA : '+current_func_name + ' , Dimension : '+str(problem_dimension))
+    plt.grid(True)
+    plt.show()
