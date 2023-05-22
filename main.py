9a05f2b87d0cff2385c5073f6825522a3b253317
DevarajMarappa <98761686+DevarajMarappa@users.noreply.github.com>
2023-05-22 01:47:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 from ypstruct import structure
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 import gaCrossOver
 import deAlgo
-
 
 pop_size = 100 #np
 crossover_rate = 0.9 #cr
 dimensions=[2,10,20]
 
-
-#GA
-# Run the genetic algorithm for each benchmark fucntions
-for bm in range(1,9):
-  # Run the genetic algorithm for each problem dimension
-  for dim in dimensions:
-      gaCrossOver.ga_logic(dim,bm)
-    
-     
 #DE
 # Run the DE for each benchmark fucntions
 for i in range(1,9):
+  process_start =None
+  process_end=None
   # Run the DE for each problem dimension
-  for dimIndex in range(len(dimensions)):
-    problem_dimension=dimensions[dimIndex]
-    deAlgo.de_logic(problem_dimension,i) #de plot are part of the DE logic implementation
+  for dim in dimensions:
+    process_start=time.perf_counter()
+    deAlgo.de_logic(dim,i) #de plot are part of the DE logic implementation
+    process_end=time.perf_counter()
+    print("DE Tot Time for Dimension : "+str(dim)+" is : "+str(process_end-process_start))
+    
+#GA
+# Run the genetic algorithm for each benchmark fucntions
+for bm in range(1,9):
+  process_start =None
+  process_end=None
+  # Run the genetic algorithm for each problem dimension
+  for dim in dimensions:
+      process_start=time.perf_counter()
+      gaCrossOver.ga_logic(dim,bm)
+      process_end=time.perf_counter()
+      print("GA Tot Time for Dimension : "+str(dim)+" is : "+str(process_end-process_start))
 
+    
+    
